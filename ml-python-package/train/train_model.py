d82a4305981511a3fbc6efdf3fcc13353d008863
Mikhail-M <mihail-maryfich@yandex.ru>
2021-05-30 17:49:07 +0300

    add files

---
+++ ml-python-package/train/train_model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+
+
+def train_model(features: pd.DataFrame, target: pd.Series, model_type: str):
+    model = build_model(model_type)
+    model.fit(features, target)
+    return model
+
+
+def build_model(model_type: str):
+    if model_type == "RandomForestRegressor":
+        model = RandomForestClassifier(
+            n_estimators=100, random_state=train_params.random_state
+        )
+    elif model_type == "LinearRegression":
+        model = LinearRegression()
+    else:
+        raise RuntimeError(f"Bad model_type -- {model_type}")
+    return model
+
+
+def serialize_model(model: object, output: str) -> str:
+    with open(output, "wb") as f:
+        pickle.dump(model, f)
+    return output
---
+++ tests/test_example.py
@@ -0,0 +1,7 @@
+def test_example():
+    assert 2 + 2 == 4
+
+
+def test_example_1():
+    assert 2 + 3 == 5
+
