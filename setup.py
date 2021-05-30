0d0ead3437fc0a686c2b3fbf38236c1e0ec2d868
Mikhail-M <mihail-maryfich@yandex.ru>
2021-05-30 18:14:15 +0300

    fix name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name="ml_example",
+    name="ml_python_package",
     packages=find_packages(),
     version="0.1.0",
     description="Training stuff",
