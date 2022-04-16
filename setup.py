b51a504aec9b6dc85fb7dd05a1413b8c23ff1833
Mikhail Maryufich <Mihail-maryfich@yandex.ru>
2022-04-16 13:02:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="ml_python_package",
     packages=find_packages(),
-    version="0.1.3",
+    version="0.1.4",
     description="Training stuff",
     author="Mikhail Maryufich",
     install_requires=["scikit-learn==0.24.1", "pandas==1.1.5",],
